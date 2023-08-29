2a53faef1dc59ccfbeb5fa16d5ef3f00b70fa0c6
ChristianBianchi99 <bianchi848@gmail.com>
2023-08-29 16:54:46 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from 'axios';
+import Main from './components/AppMain.vue';
+import {store} from './data/store.js';
+
+export default{
+  data(){
+    return{
+      store,
+    }
+  },
+  components:{
+    Main,
+  },
+  mounted(){
+    this.getProjects();
+  },
+  methods:{
+    getProjects(){
+      store.projects= '';
+      axios.get(store.apiUrl + '/api/projects').then((response) => {
+        store.projects = response.data.results;
+      });
+    }
+  }
+}
+</script>
+
+<template>
+  <Main />
+</template>
+
+<style lang="scss">
+  @use './styles/generals.scss' as *;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,36 @@
+<script>
+import {store} from '../data/store.js';
+export default{
+  data(){
+    return{
+      store,
+    }
+  },
+}
+</script>
+<template lang="">
+    <div class='container'>
+        <div class="row">
+            <div class='col-12'>
+                <div class="card" v-for='project in store.projects' :key='project.id'>
+                    <img :src="project.cover_image" class="card-img-top" :alt="project.name">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ project.name }}</h5>
+                        <p class="card-text">{{ project.description }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped lang="scss">
+.col-12{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    .card{
+        width: calc(100% / 3 - 10px);
+        margin: 15px 0;
+    }
+}
+</style>
